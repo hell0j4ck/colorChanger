12bb79bfaaea1154cb6b5537de8689834de5d5d7
Jack Jabra <jacklee.jabra@yahoo.com>
2023-08-28 23:33:32 -0400

    Reverted to normal because nothing fucking works on apple

--- app.js
+++ app.js
@@ -1,20 +1,6 @@
 const button = document.querySelector("#speak")
 const ding = new Audio("audio/ding.mp3")
 
-function unlockAudioContext(audioCtx) {
-
-  if (audioCtx.state !== 'suspended') return;
-  const b = document.body;
-  const events = ['touchstart','touchend', 'mousedown','keydown'];
-  events.forEach(e => b.addEventListener(e, unlock, false));
-  function unlock() { audioCtx.resume().then(clean); }
-  function clean() { events.forEach(e => b.removeEventListener(e, unlock)); }
-}
-
-unlockAudioContext(ding)
-
-ding.play()
-
 let speechRecognition = new webkitSpeechRecognition()
 
 speechRecognition.continuous = false;
